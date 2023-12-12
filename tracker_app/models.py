37a179ed4fa6c2261e1b9ed5b199697b98a30c00
gscerveira <61796134+gscerveira@users.noreply.github.com>
2023-12-12 22:51:37 +0000

    Add QuoteRequest model

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -29,3 +29,23 @@ class Item(models.Model):
         return self.name
 
 
+class QuoteRequest(models.Model):
+    NEED_SEND = 'need_to_send'
+    SENT = 'sent'
+    RECEIVED = 'received'
+    NEED_RESEND = 'need_to_resend'
+
+    STATUS_CHOICES = [
+        (NEED_SEND, 'Need to Send Request'),
+        (SENT, 'Request Sent'),
+        (RECEIVED, 'Quote Received'),
+        (NEED_RESEND, 'Need to Resend Request'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+    details = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Quote request for {self.item.name}"
