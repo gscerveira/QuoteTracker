196bf4c0debd8e42d4f202b7943e02d01a5f1033
gscerveira <61796134+gscerveira@users.noreply.github.com>
2023-12-12 22:23:52 +0000

    Add Item model

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -14,4 +14,18 @@ class Project(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
