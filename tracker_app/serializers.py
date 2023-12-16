afd33fbbb3e55551ec110e602c1c65ac3e8a6cf1
gscerveira <61796134+gscerveira@users.noreply.github.com>
2023-12-16 00:57:28 +0000

    Add serializers

--- tracker_app/serializers.py
+++ tracker_app/serializers.py
@@ -1,3 +1,44 @@
 from rest_framework import serializers
-from .models import User, Project, Item, QuoteRequest
+from .models import Project, Item, QuoteRequest
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True, 'validators': [validate_password]},
+            'id': {'read_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'user', 'name', 'description']
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['id', 'project', 'name', 'description']
+
+
+class QuoteRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuoteRequest
+        fields = ['id', 'item', 'status', 'details']
