81ea5a7a59ce800b27ac74ce3b8a31bfad61c524
gscerveira <61796134+gscerveira@users.noreply.github.com>
2023-12-17 22:41:20 +0000

    Refactor UserRegistrationView

--- tracker_app/views.py
+++ tracker_app/views.py
@@ -1,17 +1,12 @@
-from rest_framework import viewsets, status, views
-from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView
 from .models import Project, Item, QuoteRequest
 from .serializers import ProjectSerializer, ItemSerializer, QuoteRequestSerializer, UserSerializer
 # Create your views here.
 
 
-class UserRegistrationView(views.APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserRegistrationView(CreateAPIView):
+    serializer_class = UserSerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.prefetch_related('items', 'items__quoterequests')
