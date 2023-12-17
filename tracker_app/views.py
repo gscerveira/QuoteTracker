cf859ffa03892ab9d9e351c955bac2ca5fb74712
gscerveira <61796134+gscerveira@users.noreply.github.com>
2023-12-17 23:58:47 +0000

    Add login and logout views.

--- tracker_app/views.py
+++ tracker_app/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status, views
+from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
+from django.contrib.auth import authenticate, login, logout
 from .models import Project, Item, QuoteRequest
 from .serializers import ProjectSerializer, ItemSerializer, QuoteRequestSerializer, UserSerializer
 # Create your views here.
@@ -7,6 +9,22 @@ from .serializers import ProjectSerializer, ItemSerializer, QuoteRequestSerializ
 
 class UserRegistrationView(CreateAPIView):
     serializer_class = UserSerializer
+
+
+class UserLoginView(views.APIView):
+    def post(self, request):
+        user = authenticate(request, **request.data)
+        if user:
+            login(request, user)
+            return Response({'detail': 'Login successful.'}, status=status.HTTP_200_OK)
+        return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class UserLogoutView(views.APIView):
+    def post(self, request):
+        logout(request)
+        return Response({'detail': 'Logout successful'}, status=status.HTTP_204_NO_CONTENT)
+
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.prefetch_related('items', 'items__quoterequests')
