0de80abd7b9f4afbb4cd504379602ef67301ac4e
gscerveira <61796134+gscerveira@users.noreply.github.com>
2023-12-18 00:20:09 +0000

    Add urls for new views.

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet, ItemViewSet, QuoteRequestViewset
+from .views import ProjectViewSet, ItemViewSet, QuoteRequestViewset, UserRegistrationView, UserLoginView, UserLogoutView
 
 
 router = DefaultRouter()
@@ -9,5 +9,8 @@ router.register(r'items', ItemViewSet)
 router.register(r'quoterequests', QuoteRequestViewset)
 
 urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
     path('', include(router.urls))
 ]
