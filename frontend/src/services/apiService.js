055f257969de1b57f6beb4c195c2a7d29f5895c5
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-01-22 18:16:38 +0000

    Add GET and POST calls

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -39,12 +39,62 @@ const createProject = async (projectData) => {
 
 const fetchItems = async (projectId) => { 
     try {
-        const response = await axios.get(API_URL + 'items/?project=' + projectId);
+        const response = await axios.get(API_URL + 'items/?project=', projectId);
         return response.data;
     } catch (error) {
         console.error('Error fetching items:', error);
         throw error;
     }
-}
+};
 
-export { register, login, fetchProjects };
+const createItem = async (itemData) => {
+    try {
+        const response = await axios.post(API_URL + 'items/', itemData);
+        return response.data;
+    } catch (error) {
+        console.error('Error creating item:', error);
+        throw error;
+    }
+};
+
+const fetchQuoteRequests = async (itemId) => {
+    try {
+        const response = await axios.get(API_URL + 'quoterequests/?item=', itemId);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching quote requests:', error);
+        throw error;
+    }
+};
+
+const createQuoteRequest = async (quoteRequestData) => {
+    try {
+        const response = await axios.post(API_URL + 'quoterequests/', quoteRequestData);
+        return response.data;
+    } catch (error) {
+        console.error('Error creating quote request:', error);
+        throw error;
+    }
+};
+
+const fetchStores = async () => {
+    try {
+        const response = await axios.get(API_URL + 'stores/');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching stores:', error);
+        throw error;
+    }
+};
+
+const createStore = async (storeData) => {
+    try {
+        const response = await axios.post(API_URL + 'stores/', storeData);
+        return response.data;
+    } catch (error) {
+        console.error('Error creating store:', error);
+        throw error;
+    }
+};
+
+export { register, login, fetchProjects, createProject, fetchItems, createItem, fetchQuoteRequests, createQuoteRequest, fetchStores, createStore };
