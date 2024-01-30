7e278d417a8013a2786d85a72231de27f33ae499
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-01-30 16:27:31 +0000

    Add logic for item form

--- frontend/src/components/GenericDialog.js
+++ frontend/src/components/GenericDialog.js
@@ -1,32 +1,57 @@
 import React from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField,
-Select, MenuItem, InputLabel, FormControl } from '@mui/material';
+import {
+    Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField,
+    Select, MenuItem, InputLabel, FormControl
+} from '@mui/material';
+import { Form } from 'react-router-dom';
 
-const GenericDialog = ({ open, handleClose, title, fields, handleSubmit, handleChange }) => {
+const GenericDialog = ({ open, handleClose, title, fields, handleSubmit, handleChange, selectOptions }) => {
     return (
         <Dialog open={open} onClose={handleClose}>
             <DialogTitle>{title}</DialogTitle>
             <DialogContent>
-                {fields.map((field, index) => (
-                    <TextField
-                        key={index}
-                        margin="dense"
-                        id={field.id}
-                        label={field.label}
-                        type={field.type}
-                        fullWidth
-                        variant="standard"
-                        name={field.name}
-                        value={field.value}
-                        onChange={handleChange}
-                    />
-                ))}
+                {fields.map((field, index) => {
+                    if (field.type === 'select') {
+                        return (
+                            <FormControl fullWidth margin="dense" key={index}>
+                                <InputLabel id={`${field.id}-label`}>{field.label}</InputLabel>
+                                <Select
+                                    labelId={`${field.id}-label`}
+                                    id={field.id}
+                                    name={field.name}
+                                    value={field.value}
+                                    label={field.label}
+                                    onChange={handleChange}
+                                >
+                                    {selectOptions[field.name].map((option, optionIndex) => (
+                                        <MenuItem key={optionIndex} value={option.value}>{option.label}</MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        );
+                    } else {
+                        return (
+                            <TextField
+                                key={index}
+                                margin="dense"
+                                id={field.id}
+                                label={field.label}
+                                type={field.type}
+                                fullWidth
+                                variant="standard"
+                                name={field.name}
+                                value={field.value}
+                                onChange={handleChange}
+                            />
+                        );
+                    }
+                })}
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose}>Cancel</Button>
                 <Button onClick={handleSubmit}>Submit</Button>
             </DialogActions>
-        </Dialog>
+        </Dialog >
     );
 };
 
