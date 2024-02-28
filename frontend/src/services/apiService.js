c2359252648d0fde98f2f20159857793bfb7ecab
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-02-28 11:00:45 +0000

    fix naming of env variable

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 const getCsrfToken = () => {
     return Cookies.get('csrftoken');
